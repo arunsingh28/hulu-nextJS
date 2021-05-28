8b714f36d2d714f2afde013b783f5512fba3db73
Arun Singh <arunsingh28aug.as@gmail.com>
2021-05-29 00:01:41 +0530

    navbar

--- components/Header.js
+++ components/Header.js
@@ -3,13 +3,18 @@ import Image from 'next/image'
 import HeaderItem from './HeaderItem'
 const Header = () => {
     return (
-        <header className="">
-            <div>
-                <HeaderItem title="home" Icon={HomeIcon} />
+        <header className="flex flex-col justify-between sm:flex-row m-5 items-center h-auto">
+            <div className="flex flex-grow justify-evenly max-w-2xl">
+                <HeaderItem title="HOME" Icon={HomeIcon} />
+                <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+                <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+                <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+                <HeaderItem title="SEARCH" Icon={SearchIcon} />
+                <HeaderItem title="USER" Icon={UserIcon} />
             </div>
 
             <Image
-                className=""
+                className="object-contain"
                 src="https://links.papareact.com/ua6" width="200" height="100" />
         </header>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
 
 export default function Home() {
   return (
@@ -16,7 +15,7 @@ export default function Home() {
       <Header />
 
       {/* nav */}
-
+      <Nav/>
 
     </>
   )
